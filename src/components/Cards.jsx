81de5930e87b6a1620507cf3216bac148bffa39e
ARYAN TIWARI <aryantiwari8954@gmai.com>
2026-03-30 17:47:04 +0530

    button added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,20 @@
 const Cards = (props) => {
-
-    const {cloudinaryImageId, name, cuisines, avgRating, areaName, costForTwo} = props.resData.info
+  const { cloudinaryImageId, name, cuisines, avgRating, areaName, costForTwo } =
+    props.resData.info;
 
   return (
     <div className="res-card">
-        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} alt="" />
-        <h3>{name}</h3>  
-        <h4>{cuisines.join(", ")}</h4>
-        <h4>{avgRating}⭐</h4>
-        <h4>{areaName}</h4>  
-        <h4>{costForTwo}</h4>   
-
+      <div className="img-wrapper">
+        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} alt="food" />
+        <button className="add-btn">ADD +</button>
+      </div>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}⭐</h4>
+      <h4>{areaName}</h4>
+      <h4>{costForTwo}</h4>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
